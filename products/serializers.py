341927dd8bed4e1a23dd49ce63a3186a252aff64
rexsheikh <56654312+rexsheikh@users.noreply.github.com>
2022-03-15 12:25:19 -0400

    Created car serializer class and file.

---
+++ products/serializers.py
@@ -0,0 +1,8 @@
+from socket import CAN_RAW
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['title','description','price','inventory_quantity']
